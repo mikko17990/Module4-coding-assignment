8f156912c07d89cdccdae9a175aa3e9c07ee9edb
mikko17990 <50828811+mikko17990@users.noreply.github.com>
2023-11-20 22:37:47 +0700

    update

--- js/script.js
+++ js/script.js
@@ -4,10 +4,10 @@ for (var i = 0; i < names.length; i++){
     var firstLetter = names[i].charAt(0).toLowerCase();
     if (firstLetter === 'j') {
         console.log("Good bye" + names[i]);
-       // jsomeNameSpeak.sayGoodbye(names[i]);
+      
     }else{
         console.log("Hello" + names[i]);
-        //someNameSpeak.sayHello(names[i]);
+        
     }
 }
 ;
